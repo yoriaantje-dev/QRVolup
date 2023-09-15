7fc552be49c1feebb75a9721a3c0591eedfc7258
Yorian Peters <ypeters@kembit.nl>
2023-09-15 17:35:18 +0200

    refactoring

--- qr_generator/main.py
+++ qr_generator/main.py
@@ -2,8 +2,12 @@
 import qrcode
 from PIL import Image, ImageFont, ImageDraw
 
-file = open("C:/Users/yoria/no_space_folder/newDev/QRVolup/qr_generator/inschrijving.txt", "r")
+root_folder = "C:/Users/yoria/no_space_folder/newDev/QRVolup"
+
+file = open(f"{root_folder}/qr_generator/inschrijving.txt", "r")
 participant_list = []
+font_calibri = ImageFont.truetype('calibri.ttf', 22)
+
 for line in file:
   participant_data = line.strip().split(";")
   praticipant = {
@@ -15,52 +19,9 @@ for line in file:
 for participant in participant_list:
     name = participant["name"]
     code = qrcode.make(name)
-    code.save(f"C:/Users/yoria/no_space_folder/newDev/QRVolup/qr_generator/codes/{name}.png")
-
-mf = ImageFont.truetype('calibri.ttf', 25)
-
-for participant in participant_list:
-    name = participant["name"]
-    i=Image.open(f"C:/Users/yoria/no_space_folder/newDev/QRVolup/qr_generator/codes/{name}.png")
-    width, height = i.size
-    im = ImageDraw.Draw(i)
-    im.text((round(width*0.2), round(height*0.88)), name, font=mf)
-    i.save(f"C:/Users/yoria/no_space_folder/newDev/QRVolup/qr_generator/codes/{name}.png")
-
-
-
- 
-# Data to be encoded
-# data = 'Maarten Visser'
-
-# Encoding data using make() function
-# yorian = qrcode.make("Yorian Peters")
-# maarten = qrcode.make("Maarten Visser")
-# sam = qrcode.make("Sam Lamerichs")
-
-# Saving as an image file
-# yorian.save('yorian.png')
-# maarten.save('C:/Users/yoria/no_space_folder/newDev/QRVolup/qr_generator/codes/maarten.png')
-# sam.save('sam.png')
-
-# user_dict = {
-#     {
-#         "firstname": "Yorian",
-#         "lastname": "Peters",
-#         "qr": "Yorian",
-#     }
-# }
-
-def MakeParticipantList():
-    flag = True
-    while flag:
-        pass
-
-def SaveQRList(Participants, QRCodes):
-    print("Saving List of QR Codes...")
-    count = 0
-    for QR in QRCodes:
-        print(f"Saving QR for {Participants[count]}: QR_{Participants[count]}")
-        QR.save(f"QR_{Participants[count]}")
-        count += 1
-    print("Done!...")
+    code.save(f"{root_folder}/qr_generator/codes/{name}.png")
+    image=Image.open(f"{root_folder}/qr_generator/codes/{name}.png")
+    width, height = image.size
+    draw_image = ImageDraw.Draw(image)
+    draw_image.text((round(width*0.2), round(height*0.88)), name, font=font_calibri)
+    image.save(f"{root_folder}/qr_generator/codes/{name}.png")
